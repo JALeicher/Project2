fb541f0caff976b91d6b53e2c0027c2eec9f8557
JALeicher <91464329+JALeicher@users.noreply.github.com>
2021-11-10 05:49:20 +0200

    Added views
    -Added future defs in views.py

--- Project2/pictureApp/views.py
+++ Project2/pictureApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    return HttpResponse("Hello there")
+
+def AccountView():
+    return HttpResponse("This the account page")
+
+def SearchView():
+    return HttpResponse("ThIs is the search")
