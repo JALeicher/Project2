52be88a029d6079a23e6a0984589dde645526c0e
JALeicher <91464329+JALeicher@users.noreply.github.com>
2021-11-16 11:15:58 +0200

    Redid all the log in and log out functionality
    To summarize:
    I was an idiot, the original changes did not end up on the repo. So I rushed them while they were still engraved into the wrinkles of my brain.

--- Project2/pictureApp/urls.py
+++ Project2/pictureApp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("/SignIn",views.signIn, name="signIn")
+    path("", views.homepage_view, name="homepage"),
+    path("login",views.login_view, name="login"),
+    path("signUp",views.signup_View, name="signUp"),
+    path("logout", views.logout_view, name ="logout"),
+    path("user",views.userpage_view, name="userpage"),
 ]
--- Project2/pictureApp/views.py
+++ Project2/pictureApp/views.py
@@ -1,10 +1,35 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 # Create your views here.
-def homepage(request):
+def homepage_view(request):
     return render(request, "picApp/homepage.html")
 
-def signIn(request):
-    return render(request, "picApp/signIn.html" )
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect(reverse("homepage"))
+        else:
+            return render(request, "picApp/login.html" )
+    else:
+        return render(request, "picApp/login.html" )
 
+def signup_View(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']
+    
+    return render(request, "picApp/signUp.html" )
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("homepage"))
+
+def userpage_view(request):
+    return render(request,"picApp/userpage.html")
